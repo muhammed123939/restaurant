3f0713995c9b7802fd5b7ea6e68cc66518eadad1
muhammed123939 <mohammed123939@miuegypt.edu.eg>
2026-06-09 14:09:15 +0300

    Fix environment and API config

--- API/obj/Release/net8.0/API.AssemblyInfo.cs
+++ API/obj/Release/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ada0fc9cb728d5c5c819ed04bf0a5f2b06be6dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c818528042813e7772c53248389f575a8355de7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/environments/environment.development.ts
+++ client/src/app/environments/environment.development.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//      production : false , 
+//     apiUrl: 'http://localhost:5001/api/'
+// };
 export const environment = {
-     production : false , 
-    apiUrl: 'http://localhost:5001/api/'
+  production: true,
+  apiUrl: 'https://restaurantdb12345.azurewebsites.net/api/'
 };
